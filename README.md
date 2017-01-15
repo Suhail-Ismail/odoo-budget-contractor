7b1de7de4cc34ec6a83d09ae14b33b32712a32a6
Marc Philippe de Villeres <mpdevilleres@gmail.com>
2017-01-15 13:09:50 +0000

    update readme.md and manifest

--- README.md
+++ README.md
@@ -9,4 +9,10 @@ Contractor Management
 - Contract
 - Contractor
 - Contractor Contact
-
+- Ready for Service
+- Milestone
+- Section Inherit
+- Access Users
+        - Dependent - Can readonly
+        - User - General Usage except delete power, can Edit recurrence but not create
+        - Manager - All power to manipulate data
--- __manifest__.py
+++ __manifest__.py
@@ -6,14 +6,24 @@
     'sequence': 2,
     'description': """
 Odoo Module
-===========
+-----------
+
 Specifically Designed for Etisalat-TBPC
 
 Contractor Management
 ---------------------
+
 - Contract
 - Contractor
 - Contractor Contact
+- Ready for Service
+- Milestone
+- Section Inherit
+- Access Users
+        - Dependent - Can readonly
+        - User - General Usage except delete power, can Edit recurrence but not create
+        - Manager - All power to manipulate data
+
     """,
     'author': "Marc Philippe de Villeres",
     'website': "https://github.com/mpdevilleres",
