41058f4ea7bbbaeaba010ef09cfc98f8b55a66c3
Marc Philippe de Villeres <mpdevilleres@gmail.com>
2017-06-06 15:04:00 +0400

    Refactor

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import http
+from odoo import http
 
 # class BudgetContractor(http.Controller):
 #     @http.route('/budget_contractor/budget_contractor/', auth='public')
